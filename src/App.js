322c4b59d1b42846e606931f515af3e4ee312b69
albatros444 <lionelandresovich@mail.ru>
2023-10-16 02:34:34 +0400

    start of the project and few fetch functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [genres, setGenres] = useState(null);
+  const [movies, setMovies] = useState(null);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNjdjNzkwZmVkNTI4ZTQ2YzE5YTE1OTg4NWI1Yjc1OSIsInN1YiI6IjY1MjMwMjgzYjNmNmY1MDEzOTMyZThiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VAtPOlmzxoYso6wvi9rCS3dTlCAiIevp4J1aSt5p2gk",
+    },
+  };
+  useEffect(() => {
+    const fetchGenre = async () => {
+      fetch(
+        "https://api.themoviedb.org/3/genre/movie/list?language=en",
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          setGenres(response.genres);
+        })
+        .catch((err) => console.error(err));
+    };
+    fetchGenre();
+  }, []);
+
+  const fetchMovies = (genreN) => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?&language=en-US&with_genres=${genreN}`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.results);
+        setMovies(res.results);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  // movies?.forEach((movie) => {
+  //   console.log(movie.backdrop_path)
+  // })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ margin: "15px" }}>
+          {genres?.map((genre) => (
+            <button
+              style={{ marginRight: "10px" }}
+              onClick={() => fetchMovies(genre.id)}
+              key={genre.name}
+            >
+              {genre.name}
+            </button>
+          ))}
+        </div>
+        <div>
+          {movies?.map((movie) => (
+            <img
+              src={`http://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+              alt="kdjlsk"
+            ></img>
+          ))}
+        </div>
       </header>
     </div>
   );
